747038a3f8ca0ebeb801fc4525a21df0c5dd492b
H3721N <herlingomezescobar@gmail.com>
2024-12-20 11:33:28 -0600

    Se agregan las verificaciones de verificar token y rol de usuario

--- src/categoriaProductos/categoriaProducto.routes.js
+++ src/categoriaProductos/categoriaProducto.routes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 const { postCategoriaProducto, updateCategoriaProducto } = require('./categoriaProducto.controller');
+const { checkToken } = require('../middleware/checkToken');
+const { checkRoleAuth } = require('../middleware/checkRoleAuth');
 
-router.post('/categoriaProducto', postCategoriaProducto);
+router.post(
+    '/categoriaProducto',
+    checkToken,
+    checkRoleAuth(),
+    postCategoriaProducto
+);
 
 router.put('/categoriaProducto/:id', updateCategoriaProducto);
 
