31e06216c0994047742937622561a2925cba918a
H3721N <herlingomezescobar@gmail.com>
2024-12-22 20:21:38 -0600

    ahora las rutas verificaran si el usuario tiene el rol necesario para crear o editar registros

--- src/rol/rol.router.js
+++ src/rol/rol.router.js
@@ -4,14 +4,25 @@ const { getRoles,
         createRol,
         updateRol} = require('./rol.controller');
 const {where} = require("sequelize");
+const {checkToken} = require("../middleware/checkToken");
+const {validateRol} = require("../validators/validators");
+const {checkRoleAuth} = require("../middleware/checkRoleAuth");
 
 router.get('/rol', getRoles);
 
 router.get('/rol/:id', getRolById);
 
-router.post('/rol', createRol);
+router.post('/rol',
+    checkToken,
+    validateRol,
+    checkRoleAuth(['Admin']),
+    createRol);
 
-router.put('/rol/:id', updateRol);
+router.put('/rol/:id',
+    checkToken,
+    validateRol,
+    checkRoleAuth(['Admin']),
+    updateRol);
 
 
 module.exports = router;
