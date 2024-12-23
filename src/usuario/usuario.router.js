8f0fb475704f019a35aa7c9b3c4e83c661169685
H3721N <herlingomezescobar@gmail.com>
2024-12-22 20:21:12 -0600

    ahora las rutas verificaran si el usuario tiene el rol necesario para crear o editar registros

--- src/usuario/usuario.router.js
+++ src/usuario/usuario.router.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 const { postUsuario,
         updateUsuario } = require('./usuario.controller');
+const {checkToken} = require("../middleware/checkToken");
+const {validateUsuarios} = require("../validators/validators");
+const {checkRoleAuth} = require("../middleware/checkRoleAuth");
 
-router.post('/usuario', postUsuario);
+router.post('/usuario',
+    checkToken,
+    validateUsuarios,
+    checkRoleAuth(['Admin']),
+    postUsuario);
 
-router.put('/usuario/:id', updateUsuario);
+router.put('/usuario/:id',
+    checkToken,
+    validateUsuarios,
+    checkRoleAuth(['Admin']),
+    updateUsuario);
 
 module.exports = router;
