17064d773ebe5007bc4923efa91e7886a4fcb1b6
H3721N <herlingomezescobar@gmail.com>
2024-12-22 20:14:11 -0600

    Las rutas mandan a llamar a los metodos que verifican que se cumplan las validaciones y si el usuario tiene el rol adecuado para realizarlas

--- src/producto/producto.routes.js
+++ src/producto/producto.routes.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 const { postProducto, updateProducto } = require('./producto.controller');
+const {checkToken} = require("../middleware/checkToken");
+const {validateProducto} = require("../validators/validators");
+const {checkRoleAuth} = require("../middleware/checkRoleAuth");
 
-router.post('/producto', postProducto);
+router.post('/producto',
+    checkToken,
+    validateProducto,
+    checkRoleAuth(['Admin']),
+    postProducto);
 
-router.put('/producto/:id', updateProducto);
+router.put('/producto/:id',
+    checkToken,
+    validateProducto,
+    checkRoleAuth(['Admin']),
+    updateProducto);
 
 module.exports = router;
