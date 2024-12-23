3b6f7586817991b856b287388f6a8aa62d191144
H3721N <herlingomezescobar@gmail.com>
2024-12-22 19:49:41 -0600

    Se agrega a los llamados a los metodos que validan el rol dee usuario, para realizar una accion en el crear y editar de clientes

--- src/clientes/cliente.router.js
+++ src/clientes/cliente.router.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
 const { postCliente,
         updateCliente } = require('./cliente.controller');
+const {checkToken} = require("../middleware/checkToken");
+const { validateClientes} = require("../validators/validators");
+const {checkRoleAuth} = require("../middleware/checkRoleAuth");
 
-router.post('/cliente', postCliente);
+router.post('/cliente',
+    checkToken,
+    validateClientes,
+    checkRoleAuth(['Admin']),
+    postCliente);
 
-router.put('/cliente/:id', updateCliente);
+router.put('/cliente/:id',
+    checkToken,
+    validateClientes,
+    checkRoleAuth(['Admin']),
+    updateCliente);
 
 module.exports = router;
