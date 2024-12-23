88c13957ab63dbdcc97974ec03a262f0c1ec6552
H3721N <herlingomezescobar@gmail.com>
2024-12-22 19:45:06 -0600

    Ahora el login cuenta con la capacidad de enviar el rol de usuario al token

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,7 +1,7 @@
-//const { httpError } = require('../helpers/handleError');
 const { encrypt, compare } = require('../helpers/handleBcrypt');
 const { tokenSign } = require('../helpers/generarToken');
 const Usuario = require('../usuario/usuario.model');
+const Rol = require('../rol/rol.model');
 const bcrypt = require('bcryptjs');
 
 const loginCtrl = async (req, res) => {
@@ -10,6 +10,22 @@ const loginCtrl = async (req, res) => {
 
         const user = await Usuario.findOne({where: {email: dataLogin.email}});
         console.log(' user = ', user);
+
+        ////
+        const rolUsuario = user.idRol;
+
+        console.log('rolUsuario = ', rolUsuario);
+
+        const userRole = await Rol.findOne({
+            where: {
+                id: rolUsuario,
+            }
+        });
+        console.log('userRole in ', userRole.nombre)
+
+        rol = userRole.nombre;
+
+        ////
 
         if (!user){
             return res.status(404).json({ error: 'Usuario no encontrado' });
@@ -20,7 +36,7 @@ const loginCtrl = async (req, res) => {
             return res.status(401).json({ error: 'contraseña incorrecta' });
         }
 
-        const tokenSession = await tokenSign(user);
+        const tokenSession = await tokenSign(user, rol);
 
         return res.status(200).json({
             message: 'inicio de sesión exitoso',
