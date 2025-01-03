97390bcc626af7c490d133b0541c8a646a912ded
H3721N <herlingomezescobar@gmail.com>
2025-01-03 02:56:21 -0600

    Se cambia el medodo de verificar rol

--- src/middleware/checkRoleAuth.js
+++ src/middleware/checkRoleAuth.js
@@ -1,33 +1,43 @@
 const { verifyToken } = require('../helpers/generarToken');
-const user = require('../usuario/usuario.model');
-const role = require ('../rol/rol.model')
-const {where} = require("sequelize");
+const User = require('../usuario/usuario.model');
+const { where } = require("sequelize");
 
-const checkRoleAuth = (rol) => async (req, res, next) => {
-    try{
+const checkRoleAuth = (roles) => async (req, res, next) => {
+    try {
         const token = req.headers.authorization.split(' ').pop();
-        console.log('token = ', token)
+        console.log('token = ', token);
         const tokenData = await verifyToken(token);
 
-        console.log(tokenData)
+        console.log(tokenData);
 
         if (!tokenData) {
             return res.status(401).send({ error: 'Invalid token' });
         }
 
-        console.log('rol recivido = ', rol)
+        console.log('rol recibido = ', roles);
 
-        if (tokenData.userRol === 'Admin' || tokenData.userRol === 'Administrador') {
+        const user = await User.findOne({ where: { id: tokenData.id } });
+
+        if (!user) {
+            return res.status(401).send({ error: 'User not found' });
+        }
+
+        req.user = user;
+        req.user.rol = tokenData.userRol;
+
+        console.log('rol del usuario = ', tokenData.userRol);
+        console.log('roles permitidos = ', roles);
+
+        if (roles.includes(tokenData.userRol)) {
             next();
         } else {
-            res.status(401).send({ error: 'Este usuario no cuenta con el permiso para realizar esta accion' });
+            res.status(401).send({ error: 'Este usuario no cuenta con el permiso para realizar esta acción' });
         }
-    }catch(error){
-        res.status(401);
-        res.send({error: 'Este usuario no cuenta con el permiso para realizar esta accion'})
+    } catch (error) {
+        res.status(401).send({ error: 'Este usuario no cuenta con el permiso para realizar esta acción' });
     }
-}
+};
 
 module.exports = {
     checkRoleAuth
-}
+};
